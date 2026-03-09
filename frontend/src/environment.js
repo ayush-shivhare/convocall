00845feea9548fa2c346b11dc0921e53730d0469
Ayush Shivhare <ayushshivhare649@gmail.com>
2026-03-09 19:52:28 +0530

    DEPLOYING FRONTEND

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,8 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://convovideocallbackend.onrender.com" :
+
+    "http://localhost:8000"
+
+
 export default server;
